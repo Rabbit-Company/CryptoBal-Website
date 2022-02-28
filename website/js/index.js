f3f9b1f5d979a571be3983e01a6595b170ce44c8
ziga <ziga.zajc007@gmail.com>
2022-02-28 19:45:37 +0100

    Changed default colors of switch

--- website/js/index.js
+++ website/js/index.js
@@ -22,12 +22,12 @@ document.getElementById("graph-btn").addEventListener('click', () => {
 	if(button.ariaChecked == "false"){
 		button.ariaChecked = "true";
 		button.className = "bg-indigo-600 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none";
-		animation.className = "translate-x-5 pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200";
+		animation.className = "translate-x-5 pointer-events-none inline-block h-5 w-5 rounded-full bg-gray-100 shadow transform ring-0 transition ease-in-out duration-200";
 		return;
 	}
 	button.ariaChecked = "false";
-	button.className = "bg-gray-200 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none";
-	animation.className = "translate-x-0 pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200";
+	button.className = "bg-gray-700 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none";
+	animation.className = "translate-x-0 pointer-events-none inline-block h-5 w-5 rounded-full bg-gray-100 shadow transform ring-0 transition ease-in-out duration-200";
 });
 
 document.getElementById("add-btn").addEventListener('click', () => {
